c23fc3ac1acd4f1a1bbb75bc7f7c8678bf9f23e4
Lois Vanessa Adams <loisvanessaadams@gmail.com>
2026-08-04 20:38:02 -0100

    euclidean distance calculation

--- Similarities.py
+++ Similarities.py
@@ -1,2 +1,13 @@
 
 #Start
+import numpy as np
+def euclidean_distance(user1_ratings, user2_ratings):
+    common_ratings = (user1_ratings > 0) & (user2_ratings > 0)
+    if common_ratings.sum() == 0:
+        return np.inf
+    user1_common = user1_ratings[common_ratings]
+    user2_common = user2_ratings[common_ratings]
+
+    distance = np.sqrt(np.sum((user1_common - user2_common) ** 2))
+
+    return distance

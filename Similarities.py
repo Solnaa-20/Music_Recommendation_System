f471e5e88e603ef342b5892fb74c1ea0c305997a
Lois Vanessa Adams <loisvanessaadams@gmail.com>
2026-08-04 20:46:26 -0100

    calculate_all_distances methof

--- Similarities.py
+++ Similarities.py
@@ -1,5 +1,3 @@
-
-#Start
 import numpy as np
 def euclidean_distance(user1_ratings, user2_ratings):
     common_ratings = (user1_ratings > 0) & (user2_ratings > 0)
@@ -11,3 +9,20 @@ def euclidean_distance(user1_ratings, user2_ratings):
     distance = np.sqrt(np.sum((user1_common - user2_common) ** 2))
 
     return distance
+
+def calculate_all_distances(target_user,ratings_df):
+    distances = {}
+    target_ratings = ratings_df.loc[target_user]
+
+    for other_user in ratings_df.index:
+        if other_user == target_user:
+            continue
+
+        other_ratings = ratings_df.loc[other_user]
+
+        distance = euclidean_distance(
+            target_ratings,
+            other_ratings
+        )
+        distances[other_user] = distance
+    return distances

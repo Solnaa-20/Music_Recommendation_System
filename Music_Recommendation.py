4cb680e1bda6b8d43a44d934b89adfbb97703228
Adelle Solange Naa Ayele Hammond <adelle.hammond@ashesi.edu.gh>
2026-08-04 18:30:28 +0000

    UI skeleton

--- Music_Recommendation.py
+++ Music_Recommendation.py
@@ -1,4 +1,26 @@
 import numpy as np 
 import pandas as pd 
+import streamlit as st
 
+
+
+
+#Streamlit UI
+st.title(" Music Recommendation System")
+name = st.text_input("Enter a name")
+
+if st.button("Recommend Songs"):
+    if name:
+        songs = get_recommendations(name)
+        st.subheader(f"Recommended Songs for {name}")
+
+        for song in songs:
+            st.write(f"{song}")
+
+    else:
+        st.warning("Please enter a name")
+
+if st.checkbox("Show average Ratings"):
+    ratings = get_average_ratings()
+    st.bar_chart(ratings)
 
